420173aac2324f23d852196d049943f7db1f69a9
mightymjolnir <mightymjolnir@qq.com>
2024-12-05 11:21:42 +0800

    feat(utils/registration.py): 拆分GICP配准功能函数，添加未完成的带评估功能的配准。

--- utils/registration.py
+++ utils/registration.py
@@ -1,27 +1,87 @@
-from typing import Optional
+from typing import Iterable, List, NamedTuple, Optional, Tuple, Union
 
 import numpy as np
 import small_gicp
 
 
-def GICP_registration(source: np.ndarray, target: np.ndarray, init_T: Optional[np.ndarray] = None):
+class RegistrationResult(NamedTuple):
+    source: np.ndarray
+    target: np.ndarray
+    T_target_source: np.ndarray
+
+
+def GICP_registration(
+    source: np.ndarray,
+    target: np.ndarray,
+    init_T: Optional[np.ndarray] = None,
+    resolution: float = 0.02,
+):
     """
+    执行单次 GICP 配准
     inputs:
         source: np.ndarray (N, 3)
         target: np.ndarray (N, 3)
+        init_T: np.ndarray (4, 4) 初始位姿变换矩阵，默认为单位矩阵
+        resolution: 配准的降采样分辨率，默认为 (0.25, 0.1, 0.02)，可以传入单个 float 执行单次配准。
     return:
         transformation: np.ndarray (4, 4)
         registration_result: small_gicp.RegistrationResult
     """
-    T = init_T or np.eye(4)
-    for s in (0.25, 0.1, 0.02):
-        result = small_gicp.align(
-            target,
-            source,
-            init_T_target_source=T,
-            registration_type="GICP",
-            downsampling_resolution=s,
-            max_correspondence_distance=2 * s,
-        )
-        T = result.T_target_source
+    init_T = np.eye(4) if init_T is None else init_T
+    result = small_gicp.align(
+        target,
+        source,
+        init_T_target_source=init_T,
+        registration_type="GICP",
+        downsampling_resolution=resolution,
+        max_correspondence_distance=2 * resolution,
+        num_threads=2,
+    )
     return result.T_target_source, result
+
+
+def GICP_registrations(source: np.ndarray, target: np.ndarray, init_T: Optional[np.ndarray] = None):
+    """以 (0.25, 0.1, 0.02) 的分辨率执行分层 GICP 配准，稳定性更高"""
+    T = init_T or np.eye(4)
+    for r in (0.25, 0.1, 0.02):
+        T, result = GICP_registration(source, target, init_T=T, resolution=r)
+    return T, result
+
+
+def GICP_registration_with_evaluation(
+    source: np.ndarray,
+    target: np.ndarray,
+    init_T: np.ndarray = np.eye(4),
+    resolution: float = 0.02,
+):
+    """
+    执行 GICP 配准，同时计算配准相关的评价指标
+    [hang 先不手动实现评价指标的计算了]
+    """
+    raise NotImplementedError("还未完工")
+    target, target_tree = small_gicp.preprocess_points(target, downsampling_resolution=resolution)
+    source, source_tree = small_gicp.preprocess_points(source, downsampling_resolution=resolution)
+    # `target` and `source` are small_gicp.PointCloud with the following methods
+    # target.size()           # Number of points
+    # target.points()         # Nx4 numpy array   [x, y, z, 1] x N
+    # target.normals()        # Nx4 numpy array   [nx, ny, nz, 0] x N
+    # target.covs()           # Array of 4x4 covariance matrices
+
+    result = small_gicp.align(
+        target,
+        source,
+        target_tree,
+        init_T_target_source=init_T,
+        registration_type="GICP",
+        max_correspondence_distance=2 * resolution,
+        num_threads=2,
+    )
+
+    return {
+        "T": result.T_target_source,
+        "source": source,
+        "target": target,
+        "source_tree": source_tree,
+        "target_tree": target_tree,
+        "result": result,
+    }
