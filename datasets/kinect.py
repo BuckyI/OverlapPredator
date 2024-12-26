09760880d0c37234e7f032fc34fe6f586566e43e
mightymjolnir <mightymjolnir@qq.com>
2024-12-26 10:33:48 +0800

    feat(kinect): 支持字符串索引并优化类型检查

--- datasets/kinect.py
+++ datasets/kinect.py
@@ -3,7 +3,7 @@ Azure Kinect Dataset
 """
 
 from pathlib import Path
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Union
 
 import h5py
 import matplotlib.pyplot as plt
@@ -37,17 +37,23 @@ class KinectDataset:
         self.height = attrs["height"]
         self.K = attrs["K"]
 
-        self.hdf5: h5py.Group = self.cache.hdf5.get(self.dataset_id)
-        assert isinstance(self.hdf5, h5py.Group)
-        self.timestamps = list(map(int, self.hdf5.keys()))
+        hdf5 = self.cache.hdf5.get(self.dataset_id)
+        assert isinstance(hdf5, h5py.Group)
+        self.hdf5 = hdf5
+        self.timestamps = list(map(int, hdf5.keys()))
 
-    def __getitem__(self, idx: int) -> Frame:
-        if idx < 0 or idx >= len(self.timestamps):
-            raise IndexError(f"Index {idx} out of range")
+    def __getitem__(self, idx: Union[int, str]) -> Frame:
+        if isinstance(idx, str):
+            timestamp = int(idx)
+        elif isinstance(idx, int):
+            if idx < 0 or idx >= len(self.timestamps):
+                raise IndexError(f"Index {idx} out of range")
+            timestamp = self.timestamps[idx]
+        else:
+            raise TypeError(f"Index must be int or str, not {type(idx)}")
 
-        timestamp = self.timestamps[idx]
         g = self.hdf5.get(str(timestamp))
-
+        assert isinstance(g, h5py.Group)
         type_ = g.attrs.get("type", "")
         depth = np.asarray(g["frame_depth"])
         color = np.asarray(g["frame_color"])
