380855c0bb2e64ae6329867b58b5558d8330024f
mightymjolnir <mightymjolnir@qq.com>
2025-02-24 20:33:38 +0800

    feat(log): 增加日志格式配置及简化函数参数

--- utils/log.py
+++ utils/log.py
@@ -2,10 +2,15 @@ import sys
 
 from loguru import logger
 
+log_formats = [
+    "<light-yellow>[{level}]</> {message}",
+    "<level>[{level}]</> {message} <light-yellow>({function}:{line})</>",
+]
 
-def simplify():
+
+def simplify(format: str = log_formats[0]):
     # remove the default handlers
     if 0 in logger._core.handlers:
         logger.remove(0)
-    logger.add(sys.stderr, format="<light-yellow>[{level}]</> {message}")
+    logger.add(sys.stderr, format=format)
     logger.info("Log format simplified :)")
